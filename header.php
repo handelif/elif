b28e762813a722c8391f66bf97add9a773616cf0
Hande <handelif22@hotmail.com>
2022-04-20 14:30:05 +0300

    düzenlemeler

--- header.php
+++ header.php
@@ -1,7 +1,5 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset "UTF-8"' );?>">
+
+    <meta charset="<?php bloginfo( 'charset "UTF-8"' );?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="profile" href="https://gmpg.org/xfn/11">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style.css">
