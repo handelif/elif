5cb1b7e859a50b3dceb6e34f0f148f32992a02ce
Hande <handelif22@hotmail.com>
2022-05-06 15:40:59 +0300

    düzenlemeler

--- header.php
+++ header.php
@@ -1,9 +1,13 @@
-
-    <meta charset="<?php bloginfo( 'charset "UTF-8"' );?>">
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="profile" href="https://gmpg.org/xfn/11">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style.css">
-<?php wp_head(); ?>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
